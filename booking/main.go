2f0dec39b458499a0931e198f20ddad33ef02e10
Steelovec <pewdipieegammer@gmail.com>
2026-03-08 22:01:16 +0100

    second

--- booking/main.go
+++ booking/main.go
@@ -5,17 +5,40 @@ import(
 )
 
 func main(){
-	var conferenceName = "Go conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50 
-		
-	fmt.Printf("Welcome to %v booking app \n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v tickets are remaining \n", conferenceTickets, remainingTickets)
+
+	// alternative way of declaring vars, cant do const and the explicit types like uint
+	conferenceName := "Go conference"
+	const conferenceTickets int = 50
+	// this wont be ever negative bc of unit
+	var remainingTickets uint = 50 
+	
+	fmt.Printf("Welcome to %v booking app. \n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v tickets are remaining. \n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here!")
 
-	var userName = ""
-	// ask user for their name
+	// gives the var a data type
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 
-	userName = "Tom"
-	fmt.Println(userName)
+	fmt.Println("Enter your first name: ")
+	// has to be a pointer
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)	
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter how many tickets you want: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets, you will receive a confirmation email at %v. \n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)	
 }
+
+// 56 min
