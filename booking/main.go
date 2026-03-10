bb87b235c5d034cb472b0af123da2568da1f566d
Steelovec <pewdipieegammer@gmail.com>
2026-03-10 21:54:18 +0100

    6

--- booking/main.go
+++ booking/main.go
@@ -21,56 +21,70 @@ func main(){
 	fmt.Println("Get your tickets here!")
 
 	for {
-		// gives the var a data type
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets uint
+			// gives the var a data type
+			var firstName string
+			var lastName string
+			var email string
+			var userTickets uint
 
-		fmt.Println("Enter your first name: ")
-		// has to be a pointer
-		fmt.Scan(&firstName)
+			fmt.Println("Enter your first name: ")
+			// has to be a pointer
+			fmt.Scan(&firstName)
 
-		fmt.Println("Enter your last name: ")
-		fmt.Scan(&lastName)	
+			fmt.Println("Enter your last name: ")
+			fmt.Scan(&lastName)	
 
-		fmt.Println("Enter your email: ")
-		fmt.Scan(&email)
+			fmt.Println("Enter your email: ")
+			fmt.Scan(&email)
 
-		fmt.Println("Enter how many tickets you want: ")
-		fmt.Scan(&userTickets)
+			fmt.Println("Enter how many tickets you want: ")
+			fmt.Scan(&userTickets)
 
-		if userTickets > remainingTickets{
-			fmt.Printf("We only have %v many tickets remaining, so you cant book %v. \n", remainingTickets, userTickets)
-			continue
+			isValidName := len(firstName) >= 2 && len(lastName) >= 2
+			isValidEmail := strings.Contains(email, "@") 
+			isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+
+			if isValidName && isValidEmail && isValidTicketNumber{
+				remainingTickets = remainingTickets - userTickets
+
+				bookings = append(bookings, firstName + " " + lastName)
+
+				fmt.Printf("Thank you %v %v for booking %v tickets, you will receive a confirmation email at %v. \n", firstName, lastName, userTickets, email)
+				fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)	
+
+				// this gets the first name from the slice
+				firstNames := []string{}
+				for _, booking := range bookings{
+					var names = strings.Fields(booking)
+					firstNames = append(firstNames, names[0]) 
+				}
+
+				fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+				if remainingTickets == 0{
+					// end program
+					fmt.Println("Our conference is booked out.")
+					break
+				}
+
+			}else{
+
+				if !isValidName{
+					fmt.Println("First name or last name you entered is too short.")
+				}
+				
+				if !isValidEmail{
+					fmt.Println("Email doesnt contain @ sign.")
+				}
+
+				if !isValidTicketNumber{
+					fmt.Println("Number of tickets you entered is invalid.")
+				}
+
+			}
+
 		}
-
-		remainingTickets = remainingTickets - userTickets
-
-		bookings = append(bookings, firstName + " " + lastName)
-
-		fmt.Printf("Thank you %v %v for booking %v tickets, you will receive a confirmation email at %v. \n", firstName, lastName, userTickets, email)
-		fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)	
-
-		// this gets the first name from the slice
-		firstNames := []string{}
-		for _, booking := range bookings{
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0]) 
-		}
-
-		fmt.Printf("The first names of bookings are: %v\n", firstNames)
-
-		if remainingTickets == 0{
-			// end program
-			fmt.Println("Our conference is booked out.")
-			break
-		}else{
-
-		}
-
-	}
-
 }
 
-// 1:33
+// 1:54 switches
